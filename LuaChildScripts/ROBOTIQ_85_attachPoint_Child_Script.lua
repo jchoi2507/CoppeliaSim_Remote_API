e0e217d929f17e8d3fac9d2adc4cb45fa86eb4b9
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-06-30 17:21:28 +0900

    Create ROBOTIQ_85_attachPoint_Child_Script.lua

---
+++ LuaChildScripts/ROBOTIQ_85_attachPoint_Child_Script.lua
@@ -0,0 +1,17 @@
+function sysCall_trigger(inData)
+    -- callback function automatically added for backward compatibility
+    sim.breakForceSensor(inData.handle)
+end
+
+function sysCall_trigger(inData)
+    -- callback function automatically added for backward compatibility
+    sim.breakForceSensor(inData.handle)
+end
+
+function sysCall_init()
+end
+
+function sysCall_trigger(inData)
+    -- callback function automatically added for backward compatibility
+    sim.breakForceSensor(inData.handle)
+end

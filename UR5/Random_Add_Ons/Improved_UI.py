9b8dec64c5cdd9fd0655e5bd4697cf0d8edef1ce
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-06 18:43:22 +0900

    Update Improved_UI.py

--- UR5/Random_Add_Ons/Improved_UI.py
+++ UR5/Random_Add_Ons/Improved_UI.py
@@ -1,5 +1,9 @@
 from tkinter import * # Python UI library
 
+def incCounter():
+    global counter
+    counter = counter + 1
+
 countArr = ["", "0", "1", "2"]
 counter = 0
 
@@ -9,12 +13,11 @@ boneChickenPicture = PhotoImage(file='boneResizedWithText.png')
 bonelessChickenPicture = PhotoImage(file='bonelessResizedWithText.png')
 
 button1 = Button(root, padx=10, pady=10, image=boneChickenPicture,
-                 command=lambda: cc.boneChicken(countArr[counter]))
+                 command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()]) #Incorporating lambda is necessary in order to execute two functions INSIDE mainloop()
 button2 = Button(root, padx=10, pady=10, image=bonelessChickenPicture,
-                 command=lambda: cc.bonelessChicken(countArr[counter]))
+                 command=lambda: [cc.bonelessChicken(countArr[counter], counter), incCounter()])
 
 button1.pack()
 button2.pack()
 
 root.mainloop()
-counter = counter + 1

d58e2f5259b18675a51b046a64beb1dfbab282a0
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-06 16:23:27 +0900

    Update customizable_Table_Child_Script.lua

--- Lua_Child_Scripts/customizable_Table_Child_Script.lua
+++ Lua_Child_Scripts/customizable_Table_Child_Script.lua
@@ -21,9 +21,10 @@ local RESPONDABLE_SHAPE = 8
 local tblSize = {0.019, 0.019, 0.019}
 
 while true do
-    local hndShape = sim.createPureShape(0, VISIBLE_EDGES + RESPONDABLE_SHAPE, tblSize, 0.1, NULL)
-    sim.setObjectPosition(hndShape, -1, {-1.97, -0.7028, 0.5548})
-    sim.setObjectSpecialProperty(hndShape, sim.objectspecialproperty_detectable_all)
+    local hndShape = sim.createPureShape(0, VISIBLE_EDGES + RESPONDABLE_SHAPE, tblSize, 0.1, NULL) -- Creates the pure shape
+    sim.setObjectInt32Parameter(hndShape, 10, 0) -- Sets the pure shape as invisible from cameras ONLY, still visible from vision sensor
+    sim.setObjectPosition(hndShape, -1, {-1.97, -0.7028, 0.5548}) -- Sets spawn point of shape
+    sim.setObjectSpecialProperty(hndShape, sim.objectspecialproperty_detectable_all) -- Makes the object detectable to vision sensor
     sim.wait(10)
     
 end

c12ebcd7a59a9bc5f3638bef2a762c91d0e9186a
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-06 16:44:55 +0900

    Update UR5_Full_Simulation_4_1.py

--- UR5/Deep_Fry_Simulation_v2/UR5_Full_Simulation_4_1.py
+++ UR5/Deep_Fry_Simulation_v2/UR5_Full_Simulation_4_1.py
@@ -1,77 +1,45 @@
 # 7/5/2021 Jacob Choi
 
-# To include in lua script in Coppelia software:
+## UR5_Full_Simulation_4_1.py is the second version of the deep fry simulation. Some differences between v1 and v2
+## include:
+        # More applicability to a robotic kitchen--cooking either bone or boneless chicken
+            # 4 baskets instead of 2
+            # Proximity sensor and pseudo-conveyor application
+            # 'Infinite' basket spawning mechanism
+        # Menu UI with automated backend scheduling (no direct commands)
+            # Improved button interface
+        # Organized modules for readability and debugging purposes
+
+# To include in Lua script in Coppelia software:
 # simRemoteApi.start(19999)
 
                 ## Remote API connection ##
 
-import sys
-import time
-from colorama import Fore, Style, Back
-from tkinter import *
+from tkinter import * # Python UI library
 
-import basket # Basket functions module
 import globalvariables as g # Global variables module
-import gripper as grip # Gripper funcitons module
+import chickencooking as cc # Chicken-cooking functions module
 
-# Prints out a successful/unsuccessful connection message
-def connectionMessage(clientid):
-    if clientid != -1:
-        print("Connected to remote API server")
-    else:
-        print("Connection unsuccessful :(. Ensure simulation is already running.")
-        sys.exit()
-
-connectionMessage(g.clientID)
-
-                ## Performing the deep frying actions ##
-
-# Deep frying time: 9 minutes
-def boneChicken():
-    print(Fore.BLUE + "Preparing your boned chicken!")
-
-    grip.openGripperAtStart(g.clientID, g.j1, g.j2, g.p1, g.p2)
-    time.sleep(2)
-
-    basket.moveBasket(1)
-    time.sleep(5) #200
-    basket.shakeBasket(1)
-    time.sleep(5) #200
-    basket.shakeBasket(1)
-    time.sleep(5) #100
-    basket.returnBasket(1)
-
-    print(Fore.BLUE + "Your boned chicken is ready.")
-
-# Deep frying time: 6 minutes
-def bonelessChicken():
-    print(Fore.GREEN + "Preparing your boneless chicken!")
-
-    grip.openGripperAtStart(g.clientID, g.j1, g.j2, g.p1, g.p2)
-    time.sleep(2)
-
-    basket.moveBasket(2)
-    time.sleep(5) #120
-    basket.shakeBasket(2)
-    time.sleep(5) #120
-    basket.shakeBasket(2)
-    time.sleep(5) #120
-    basket.returnBasket(2)
-
-    print(Fore.GREEN + "Your boneless chicken is ready.")
+g.connectionMessage(g.clientID) # Printing out a successful/unsuccessful connection message
 
                 ## Simple UI ##
 
+countArr = ["", "0", "1", "2"]
+counter = 0
+
 root = Tk()
 
-boneChickenPicture = PhotoImage(file = 'boneResizedWithText.png')
-bonelessChickenPicture = PhotoImage(file = 'bonelessResizedWithText.png')
+boneChickenPicture = PhotoImage(file='boneResizedWithText.png')
+bonelessChickenPicture = PhotoImage(file='bonelessResizedWithText.png')
 
-button1 = Button(root, padx = 10, pady = 10, image = boneChickenPicture,
-                 command = lambda: boneChicken())
-button2 = Button(root, padx = 10, pady = 10, image = bonelessChickenPicture,
-                 command = lambda: bonelessChicken())
+button1 = Button(root, padx=10, pady=10, image=boneChickenPicture,
+                 command=lambda: cc.boneChicken(countArr[counter]))
+button2 = Button(root, padx=10, pady=10, image=bonelessChickenPicture,
+                 command=lambda: cc.bonelessChicken(countArr[counter]))
 
 button1.pack()
 button2.pack()
+
 root.mainloop()
+counter = counter + 1
+

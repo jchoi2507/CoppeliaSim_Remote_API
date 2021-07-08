40a8a8af0b633e8f04394196d85af922229d5f35
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-08 09:53:05 +0900

    Update basket.py

--- UR5/Deep_Fry_Simulation_v2/basket.py
+++ UR5/Deep_Fry_Simulation_v2/basket.py
@@ -1,35 +1,38 @@
 ## basket.py is the module that commands which basket to perform what action. It was created to provide more readability
 ## to the basketfunctions.py module.
 
+import threading
+
 import basketfunctions as bf # Function definitions for basket module
 import globalvariables as g # Global variables module
+import UI as ui
 
-def moveBasket(targetPosition, arrIndex):
-    if targetPosition == 1:
+def moveBasket(arrIndex, targetPosition):
+    if targetPosition == 0: #occupy position 1
         bf.moveBasketFunc(g.clientID, 1, arrIndex)
-    elif targetPosition == 2:
+    elif targetPosition == 1: #occupy position 2
         bf.moveBasketFunc(g.clientID, 2, arrIndex)
-    elif targetPosition == 3:
+    elif targetPosition == 2: #occupy position 3
         pass
-    elif targetPosition == 4:
+    elif targetPosition == 3: #occupy position 4
         pass
 
-def shakeBasket(targetPosition, arrIndex):
-    if targetPosition == 1:
+def shakeBasket(arrIndex, targetPosition):
+    if targetPosition == 0: #occupy position 1
         bf.shakeBasketFunc(g.clientID, 1, arrIndex)
-    elif targetPosition == 2:
+    elif targetPosition == 1: #occupy position 2
         bf.shakeBasketFunc(g.clientID, 2, arrIndex)
-    elif targetPosition == 3:
+    elif targetPosition == 2: #occupy position 3
         pass
-    elif targetPosition == 4:
+    elif targetPosition == 3: #occupy position 4
         pass
 
-def returnBasket(targetPosition, arrIndex):
-    if targetPosition == 1:
+def returnBasket(arrIndex, targetPosition):
+    if targetPosition == 0: #occupy position 1
         bf.returnBasketFunc(g.clientID, 1, arrIndex)
-    elif targetPosition == 2:
+    elif targetPosition == 1: #occupy position 2
         bf.returnBasketFunc(g.clientID, 2, arrIndex)
-    elif targetPosition == 3:
+    elif targetPosition == 2: #occupy position 3
         pass
-    elif targetPosition == 4:
+    elif targetPosition == 3: #occupy position 4
         pass

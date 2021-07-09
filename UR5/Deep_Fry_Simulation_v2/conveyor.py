08f9c422823e6584e8b7a1b3d537b1cc53410db9
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-09 18:11:53 +0900

    Update conveyor.py

--- UR5/Deep_Fry_Simulation_v2/conveyor.py
+++ UR5/Deep_Fry_Simulation_v2/conveyor.py
@@ -1,13 +1,11 @@
 import sim
-
 import globalvariables as g
 
-# Initializes the basket by attaching it to the invisible (by cameras in Coppelia) cube on the conveyor
+#Initializes the basket by attaching it to the invisible (by cameras in Coppelia) cube on the conveyor
 def initBasket(arrIndex):
-
-    relativeToParent = [0.18, 0, 0.058]
+    #relativeToParent = [0.18, 0, 0.058]
     errorCode, basketH = sim.simxGetObjectHandle(g.clientID, 'Basket' + arrIndex, sim.simx_opmode_blocking)
-    errorCode, cuboidH = sim.simxGetObjectHandle(g.clientID, 'Cuboid', sim.simx_opmode_blocking)
+    #errorCode, cuboidH = sim.simxGetObjectHandle(g.clientID, 'Cuboid', sim.simx_opmode_blocking)
 
-    sim.simxSetObjectParent(g.clientID, basketH, cuboidH, False, sim.simx_opmode_blocking)
-    sim.simxSetObjectPosition(g.clientID, basketH, sim.sim_handle_parent, relativeToParent, sim.simx_opmode_oneshot)
+    #sim.simxSetObjectParent(g.clientID, basketH, cuboidH, False, sim.simx_opmode_blocking)
+    sim.simxSetObjectPosition(g.clientID, basketH, -1, g.basket_spawn, sim.simx_opmode_oneshot)

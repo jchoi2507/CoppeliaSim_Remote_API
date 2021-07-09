4feddcde1cc765c4ff1ea8c95709e50108af8f6e
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-09 11:10:32 +0900

    Update Improved_UI.py

--- UR5/Random_Add_Ons/Improved_UI.py
+++ UR5/Random_Add_Ons/Improved_UI.py
@@ -1,23 +1,32 @@
-from tkinter import * # Python UI library
-
 def incCounter():
     global counter
     counter = counter + 1
 
-countArr = ["", "0", "1", "2"]
+#Continually displays timers for baskets
+def startDisplayThread():
+    tableThread = threading.Thread(target=ui.displayTimers,
+                                   args=())
+    tableThread.start()
+    return
+
+                ## Simple UI ##
+
+countArr = ["", "0", "1", "2"] #Array of appendable strings that represent the basket's ID in the CoppeliaSim scene
 counter = 0
 
 root = Tk()
 
-boneChickenPicture = PhotoImage(file='boneResizedWithText.png')
-bonelessChickenPicture = PhotoImage(file='bonelessResizedWithText.png')
+boneChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\boneResizedWithText.png")
+bonelessChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\bonelessResizedWithText.png")
 
-button1 = Button(root, padx=10, pady=10, image=boneChickenPicture,
-                 command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()]) #Incorporating lambda is necessary in order to execute two functions INSIDE mainloop()
-button2 = Button(root, padx=10, pady=10, image=bonelessChickenPicture,
+#Creating three buttons, 1) display timers option 2) order bone chicken 3) order boneless chicken
+button1 = Button(root, text="Display timers",padx=120, pady=20, command=lambda: startDisplayThread())
+button2 = Button(root, image=boneChickenPicture,
+                 command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()])
+button3 = Button(root, image=bonelessChickenPicture,
                  command=lambda: [cc.bonelessChicken(countArr[counter], counter), incCounter()])
 
 button1.pack()
 button2.pack()
-
+button3.pack()
 root.mainloop()

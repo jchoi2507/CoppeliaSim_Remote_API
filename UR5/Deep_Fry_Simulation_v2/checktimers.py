edd3c9c1af0f5ad70ea597db426429ca2702abd5
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-09 18:10:06 +0900

    Update checktimers.py

--- UR5/Deep_Fry_Simulation_v2/checktimers.py
+++ UR5/Deep_Fry_Simulation_v2/checktimers.py
@@ -1,11 +1,8 @@
 import time
-from colorama import Fore
-import threading
-
 import globalvariables as g
-import basketfunctions as bf
-import UI as ui
+import basketfunctions as bf #'Body' of the chicken-cooking functions module
 
+#To be called in a thread, this function continuously checks if the basket is ready to be shaken (up to 3 times)
 def b1checkTimer(targetPosition, arrIndex):
     numOfShakes = 0
     while True:
@@ -24,6 +21,7 @@ def b1checkTimer(targetPosition, arrIndex):
                 bf.returnBasketFunc(g.clientID, targetPosition, arrIndex)
                 break
 
+#To be called in a thread, this function continuously checks if the basket is ready to be shaken (up to 3 times)
 def b2checkTimer(targetPosition, arrIndex):
     numOfShakes = 0
     while True:
@@ -42,6 +40,7 @@ def b2checkTimer(targetPosition, arrIndex):
                 bf.returnBasketFunc(g.clientID, targetPosition, arrIndex)
                 break
 
+#To be called in a thread, this function continuously checks if the basket is ready to be shaken (up to 3 times)
 def b3checkTimer(targetPosition, arrIndex):
     numOfShakes = 0
     while True:
@@ -60,6 +59,7 @@ def b3checkTimer(targetPosition, arrIndex):
                 bf.returnBasketFunc(g.clientID, targetPosition, arrIndex)
                 break
 
+#To be called in a thread, this function continuously checks if the basket is ready to be shaken (up to 3 times)
 def b4checkTimer(targetPosition, arrIndex):
     numOfShakes = 0
     while True:
@@ -77,3 +77,20 @@ def b4checkTimer(targetPosition, arrIndex):
             if (numOfShakes == 3):
                 bf.returnBasketFunc(g.clientID, targetPosition, arrIndex)
                 break
+
+#Accounts for multiple baskets in the deep fryer system (TOTAL SHAKING TIME: ~11 SECONDS)
+def delayIfNecessary():
+    t1_left = 60.0 - (g.t1.endTime - g.t1.startTime)
+    t2_left = 60.0 - (g.t2.endTime - g.t2.startTime)
+    t3_left = 60.0 - (g.t3.endTime - g.t3.startTime)
+    t4_left = 60.0 - (g.t4.endTime - g.t4.startTime)
+
+    if (t1_left < 20.0): #The robotic arm won't move the basket until the previous basket is done shaking
+        time.sleep(t1_left + 12.0)
+    
+    elif (t2_left < 20.0):
+        time.sleep(t2_left + 12.0)
+    
+    elif (t3_left < 20.0):
+        time.sleep(t3_left + 12.0)
+

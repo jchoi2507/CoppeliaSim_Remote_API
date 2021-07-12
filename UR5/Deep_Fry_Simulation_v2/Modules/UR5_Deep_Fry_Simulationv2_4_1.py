5fe09505e5887740a89721456a1258a8d2a78788
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-12 15:22:05 +0900

    Update UR5_Deep_Fry_Simulationv2_4_1.py

--- UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
+++ UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
@@ -1,10 +1,11 @@
 # 7/5/2021 Jacob Choi
+# Main executable file
 
 from tkinter import *
 import threading
-import globalvariables as g #Global variables module
-import chickencooking as cc #Chicken-cooking functions module
-import UI as ui #Timer UI module
+import globalvariables as g
+import chickencooking as cc
+import UI as ui
 
 g.connectionMessage(g.clientID) #Printing out a successful/unsuccessful remote API connection message
 
@@ -15,8 +16,7 @@ def incCounter():
 
 #Continually displays timers for baskets
 def startDisplayThread():
-    tableThread = threading.Thread(target=ui.displayTimers,
-                                   args=())
+    tableThread = threading.Thread(target=ui.displayTimers, args=())
     tableThread.start()
     return
 
@@ -31,7 +31,7 @@ boneChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\b
 bonelessChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\bonelessResizedWithText.png")
 
 #Creating three buttons, 1) display timers option 2) order bone chicken 3) order boneless chicken
-button1 = Button(root, text="Display timers",padx=120, pady=20, command=lambda: startDisplayThread())
+button1 = Button(root, text="Start order!",padx=120, pady=20, command=lambda: startDisplayThread())
 button2 = Button(root, image=boneChickenPicture,
                  command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()])
 button3 = Button(root, image=bonelessChickenPicture,
@@ -42,24 +42,3 @@ button2.pack()
 button3.pack()
 root.mainloop()
 
-                ## Simple UI ##
-
-countArr = ["", "0", "1", "2"]
-counter = 0
-
-root = Tk()
-
-boneChickenPicture = PhotoImage(file='boneResizedWithText.png')
-bonelessChickenPicture = PhotoImage(file='bonelessResizedWithText.png')
-
-button1 = Button(root, text="Click here to start ordering.",padx=10, pady=10, command=lambda: startDisplayThread())
-button2 = Button(root, padx=10, pady=10, image=boneChickenPicture,
-                 command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()])
-button3 = Button(root, padx=10, pady=10, image=bonelessChickenPicture,
-                 command=lambda: [cc.bonelessChicken(countArr[counter], counter), incCounter()])
-
-button1.pack()
-button2.pack()
-button3.pack()
-
-root.mainloop()

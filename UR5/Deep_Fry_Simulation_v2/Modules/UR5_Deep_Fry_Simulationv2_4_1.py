ba94b86f55fa9d29fbb0e8323708eff3d90ae982
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-12 16:57:28 +0900

    Update UR5_Deep_Fry_Simulationv2_4_1.py

--- UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
+++ UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
@@ -28,17 +28,17 @@ counter = 0
 root = Tk()
 
 boneChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\boneResizedWithText.png")
-bonelessChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\bonelessResizedWithText.png")
+bonelessChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\bonelessResized2WithText.png")
 
 #Creating three buttons, 1) display timers option 2) order bone chicken 3) order boneless chicken
-button1 = Button(root, text="Start order!",padx=120, pady=20, command=lambda: startDisplayThread())
+button1 = Button(root, text="DISPLAY QUEUE TIMERS",padx=50, pady=20, command=lambda: startDisplayThread())
 button2 = Button(root, image=boneChickenPicture,
                  command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()])
 button3 = Button(root, image=bonelessChickenPicture,
                  command=lambda: [cc.bonelessChicken(countArr[counter], counter), incCounter()])
 
-button1.pack()
-button2.pack()
+button1.pack(fill="both", expand=True, padx=20, pady=20)
+button2.pack(side=LEFT) #Organize the buttons
 button3.pack()
 root.mainloop()
 

6a0e4c12b13f5010db56631e882cb0bf2291e472
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-12 15:24:25 +0900

    Update globalvariables.py

--- UR5/Deep_Fry_Simulation_v2/Modules/globalvariables.py
+++ UR5/Deep_Fry_Simulation_v2/Modules/globalvariables.py
@@ -11,7 +11,7 @@ import time
 #Table class that represents one section of one deep-fryer (in the CoppeliaSim scene, this is one-half of one of the red tables)
 class Table:
 
-    #Initialization method
+    #Constructor method
     def __init__(self):
         self.empty = True #boolean empty or not
         self.startTime = 0 #time at shake start
@@ -39,6 +39,11 @@ t3 = Table()
 t4 = Table()
 tableArr = [t1, t2, t3, t4] #Table array representing the four available spaces in the deep-fryers
 
+t1NumOfShakes = 0
+t2NumOfShakes = 0
+t3NumOfShakes = 0
+t4NumOfShakes = 0
+
                 ## Initialization ##
 
 clientID = sim.simxStart('127.0.0.1', 19999, True, True, 5000, 5)  #Server IP, Port num, boolean wait-until-connected
@@ -60,11 +65,6 @@ errorCode, j1 = sim.simxGetObjectHandle(clientID, 'ROBOTIQ_85_active1', sim.simx
 errorCode, j2 = sim.simxGetObjectHandle(clientID, 'ROBOTIQ_85_active2', sim.simx_opmode_blocking) #gripper joint 2
 errorCode, connector = sim.simxGetObjectHandle(clientID, 'ROBOTIQ_85_attachPoint',
                                                sim.simx_opmode_blocking) #gripper connect point
-errorCode, proximitySensor = sim.simxGetObjectHandle(clientID, 'Proximity_sensor',
-                                                     sim.simx_opmode_blocking) #proximity sensor
-
-returnCode, detectionState, detectedPoint, detectedObjectHandle, detectedSurfaceNormalVector = \
-    sim.simxReadProximitySensor(clientID, proximitySensor, sim.simx_opmode_streaming) #Initializing proximity sensor
 
 #Obtaining joint positions for the gripper to close & open
 errorCode, p1 = sim.simxGetJointPosition(clientID, j1, sim.simx_opmode_streaming)
@@ -72,41 +72,57 @@ errorCode, p2 = sim.simxGetJointPosition(clientID, j2, sim.simx_opmode_streaming
 
                 ## Coordinates ##
 
+#Initial + spawn point coordinates
 initial_pos = [-1.63, 0.59, 0.675, 0, 0, 0] #[x, y, z, alpha, beta, gamma]
-basket_spawn = [-1.788, 0.5912, 0.6138] # [x, y, z]
+basket_spawn = [-1.788, 0.5912, 0.6138] #[x, y, z]
 
-#Coordinates for basket 1
+#Basket-shaking coordinates
+b1_back_pos = [-1.6, 0.81, 0.52, 0, 0, 0]
+b2_back_pos = [-1.6, 0.97, 0.52, 0, 0, 0]
+b3_back_pos = [-1.6, 1.17, 0.52, 0, 0, 0]
+b4_back_pos = [-1.6, 1.3, 0.52, 0, 0, 0]
+
+#Basket-returning coordinates
+b1_return_pos = [-1.64, 0.88, 0.68, 0, 0, 0]
+b2_return_pos = [-1.64, 1.05, 0.68, 0, 0, 0]
+b3_return_pos = [-1.64, 1.2, 0.68, 0, 0, 0]
+b4_return_pos = [-1.64, 1.33, 0.68, 0, 0, 0]
+
+#Basket-moving coordinates for basket 1
 b1_int_pos = [-1.63, 0.59, 0.725, 0, 0, 0]
 b1_int_pos_2 = [-1.475, 0.65, 0.725, 0, 0, 0]
 b1_int_pos_3 = [-1.475, 0.81, 0.725, 0, 0, 0]
 b1_int_pos_4 = [-1.56, 0.81, 0.725, 0, 0, 0]
 b1_final_pos = [-1.56, 0.81, 0.52, 0, 0, 0]
 
-#Coordinates for basket 2 -- NEED TO RECONFIGURE
+#Basket-moving coordinates for basket 2
 b2_int_pos = [-1.63, 0.59, 0.725, 0, 0, 0]
 b2_int_pos_2 = [-1.475, 0.65, 0.725, 0, 0, 0]
 b2_int_pos_3 = [-1.475, 0.8, 0.725, 0, 0, 0]
-b2_int_pos_4 = [-1.475, 0.97, 0.725, 0, 0, 0]
-b2_int_pos_5 = [-1.56, 0.97, 0.725, 0, 0, 0]
+b2_int_pos_4 = [-1.475, 0.88, 0.725, 0, 0, 0]
+b2_int_pos_5 = [-1.475, 0.97, 0.725, 0, 0, 0]
+b2_int_pos_6 = [-1.56, 0.97, 0.725, 0, 0, 0]
 b2_final_pos = [-1.56, 0.97, 0.52, 0, 0, 0]
 
-#Coordinates for basket 3
+#Basket-moving coordinates for basket 3
 b3_int_pos = [-1.63, 0.59, 0.725, 0, 0, 0]
 b3_int_pos_2 = [-1.475, 0.65, 0.725, 0, 0, 0]
 b3_int_pos_3 = [-1.475, 0.8, 0.725, 0, 0, 0]
 b3_int_pos_4 = [-1.475, 0.9, 0.725, 0, 0, 0]
 b3_int_pos_5 = [-1.475, 1.0, 0.725, 0, 0, 0]
-b3_int_pos_6 = [-1.475, 1.17, 0.725, 0, 0, 0]
-b3_int_pos_7 = [-1.56, 1.17, 0.725, 0, 0, 0]
+b3_int_pos_6 = [-1.475, 1.1, 0.725, 0, 0, 0]
+b3_int_pos_7 = [-1.475, 1.17, 0.725, 0, 0, 0]
+b3_int_pos_8 = [-1.56, 1.17, 0.725, 0, 0, 0]
 b3_final_pos = [-1.56, 1.17, 0.52, 0, 0, 0]
 
-#Coordinates for basket 4
+#Basket-moving coordinates for basket 4
 b4_int_pos = [-1.63, 0.59, 0.725, 0, 0, 0]
 b4_int_pos_2 = [-1.475, 0.65, 0.725, 0, 0, 0]
 b4_int_pos_3 = [-1.475, 0.8, 0.725, 0, 0, 0]
 b4_int_pos_4 = [-1.475, 0.9, 0.725, 0, 0, 0]
 b4_int_pos_5 = [-1.475, 1.0, 0.725, 0, 0, 0]
-b4_int_pos_6 = [-1.475, 1.17, 0.725, 0, 0, 0]
-b4_int_pos_7 = [-1.475, 1.3, 0.725, 0, 0, 0]
-b4_int_pos_8 = [-1.56, 1.3, 0.725, 0, 0, 0]
+b4_int_pos_6 = [-1.475, 1.1, 0.725, 0, 0, 0]
+b4_int_pos_7 = [-1.475, 1.17, 0.725, 0, 0, 0]
+b4_int_pos_8 = [-1.475, 1.3, 0.725, 0, 0, 0]
+b4_int_pos_9 = [-1.56, 1.3, 0.725, 0, 0, 0]
 b4_final_pos = [-1.56, 1.3, 0.52, 0, 0, 0]

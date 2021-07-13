0a3e61655b78ecc0a4d35c45ef86541481db98e2
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-13 12:08:52 +0900

    Update chickencooking.py

--- UR5/Deep_Fry_Simulation_v2/Modules/chickencooking.py
+++ UR5/Deep_Fry_Simulation_v2/Modules/chickencooking.py
@@ -1,3 +1,6 @@
+# chickencooking.py acts as the link between the button interface and actual commands. It performs the spawning of the basket,
+# initialization of the appropriate basket/table class, and calls the move function for the basket.
+
 import time
 import globalvariables as g
 import basket
@@ -8,21 +11,19 @@ import checktimers
 #Performs the operation of moving the bone-chicken basket from the conveyor to the deep-fryer
 def boneChicken(arrIndex, counter):
     conveyor.initBasket(arrIndex) #Spawning a non-dynamic basket to the 'batter and breading machine' platform
+    g.tableArr[counter].type = "BONE"
+    checktimers.delayIfNecessary() #Accounting for multiple baskets in the system
 
-    checktimers.delayIfNecessary()
-
-    time.sleep(1)
     grip.openGripperAtStart(g.clientID, g.j1, g.j2, g.p1, g.p2) #Opens gripper at the very beginning of moving the basket
     time.sleep(2)
     basket.moveBasket(arrIndex, counter) #Move basket from conveyor -> table x
 
 #Performs the operation of moving the boneless-chicken basket from the conveyor to the deep-fryer
 def bonelessChicken(arrIndex, counter):
-    conveyor.initBasket(arrIndex) #Spawning a non-dynamic basket to the 'batter and breading machine' platform
-
+    conveyor.initBasket(arrIndex)
+    g.tableArr[counter].type = "BONELESS"
     checktimers.delayIfNecessary()
 
-    time.sleep(1)
-    grip.openGripperAtStart(g.clientID, g.j1, g.j2, g.p1, g.p2) #Opens gripper at the very beginning of moving the basket
+    grip.openGripperAtStart(g.clientID, g.j1, g.j2, g.p1, g.p2)
     time.sleep(2)
-    basket.moveBasket(arrIndex, counter) #Move basket from conveyor -> table x
+    basket.moveBasket(arrIndex, counter) 

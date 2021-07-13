38689f6f76477bd8a7ab7912014d6dd8e13c3653
Jacob Choi <53236140+jchoi2507@users.noreply.github.com>
2021-07-13 12:08:39 +0900

    Update UR5_Deep_Fry_Simulationv2_4_1.py

--- UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
+++ UR5/Deep_Fry_Simulation_v2/Modules/UR5_Deep_Fry_Simulationv2_4_1.py
@@ -1,5 +1,6 @@
-# 7/5/2021 Jacob Choi
-# Main executable file
+# Main executable file for version 2 of the deep fry simulation
+# By: Jacob Choi
+# 7/5/2021
 
 from tkinter import *
 import threading
@@ -20,18 +21,18 @@ def startDisplayThread():
     tableThread.start()
     return
 
-                ## Simple UI ##
-
 countArr = ["", "0", "1", "2"] #Array of appendable strings that represent the basket's ID in the CoppeliaSim scene
 counter = 0
+
+#Creating a simple UI
 
 root = Tk()
 
 boneChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\boneResizedWithText.png")
 bonelessChickenPicture = PhotoImage(file=r"C:\Users\LG\Desktop\DeepFrySimulationv2\bonelessResized2WithText.png")
 
-#Creating three buttons, 1) display timers option 2) order bone chicken 3) order boneless chicken
-button1 = Button(root, text="DISPLAY QUEUE TIMERS",padx=50, pady=20, command=lambda: startDisplayThread())
+#Creating three buttons: 1) display timers option 2) order bone chicken 3) order boneless chicken
+button1 = Button(root, text="POWER ON",padx=50, pady=20, command=lambda: startDisplayThread())
 button2 = Button(root, image=boneChickenPicture,
                  command=lambda: [cc.boneChicken(countArr[counter], counter), incCounter()])
 button3 = Button(root, image=bonelessChickenPicture,
